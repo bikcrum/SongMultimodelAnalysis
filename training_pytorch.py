50a3a379d860b97ab50fa4caedaf30654bd3006a
bikram <bikcrum@gmail.com>
2022-05-27 18:48:48 -0700

    fix mps device check

--- training_pytorch.py
+++ training_pytorch.py
@@ -17,10 +17,14 @@ from torch.utils.tensorboard import SummaryWriter
 
 if torch.cuda.is_available():
     device = torch.device("cuda")
-elif torch.backends.mps.is_available():
-    device = torch.device("mps")
 else:
-    device = "cpu"
+    try:
+        if torch.backends.mps.is_available():
+            device = torch.device("mps")
+        else:
+            device = torch.device("cpu")
+    except:
+        device = torch.device("cpu")
 
 print(f"Using device: {device}")
 
